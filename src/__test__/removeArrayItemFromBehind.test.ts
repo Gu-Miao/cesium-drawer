f139f092072508403de10c0a7658f396d0f25673
Gu-Miao <1450206741@qq.com>
2022-04-08 16:30:36 +0800

    build: remove jest

--- src/__test__/removeArrayItemFromBehind.test.ts
+++
@@ -1,7 +0,0 @@
-import { removeArrayItemFromBehind } from '../utils'
-
-test('removeArrayItemFromBehind()', () => {
-  expect(removeArrayItemFromBehind([1, 2, 3, 4, 5], 5).join('')).toEqual('1234')
-  expect(removeArrayItemFromBehind([true, 2, 'sadf'], 5).length).toEqual(3)
-  expect(removeArrayItemFromBehind([10], 5).length).toEqual(1)
-})
